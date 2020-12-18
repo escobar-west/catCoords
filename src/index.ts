00b6f6c7ee40130f65fa71997a9a46cb6ecabac3
Victor Suriel <victorv.suriel@gmail.com>
2020-12-18 07:54:08 -0500

    final deployment for testing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { ethers } from 'ethers'
-import { JsonRpcProvider, Web3Provider, ExternalProvider } from '@ethersproject/providers'
-import artifact from './resources/CatMap.json'
+import { ethers } from 'ethers';
+import { JsonRpcProvider, Web3Provider, ExternalProvider, TransactionResponse } from '@ethersproject/providers';
+import { Signer } from '@ethersproject/abstract-signer';
+import artifact from './resources/CatMap.json';
 
 export class ContractIFace {
     provider: JsonRpcProvider;
     contract: ethers.Contract;
 
     constructor(provider?: ExternalProvider) {
-        if (provider == undefined) {
+        if (provider === undefined) {
             this.provider = new JsonRpcProvider("http:\/\/127.0.0.1:8545");
         } else {
             this.provider = new Web3Provider(provider);
@@ -15,7 +16,7 @@ export class ContractIFace {
         this.contract = new ethers.Contract(
             artifact.contractAddress,
             artifact.abi,
-            this.provider.getSigner()
+            this.provider
         );
     }
 
@@ -29,30 +30,34 @@ export class ContractIFace {
 
     async getIdCoords(id: number): Promise<[number, number]> {
         try {
-            return [2,2];
+            const binCoords = await this.contract.catIdToCoordinate(id);
+            if (binCoords === "0x") {
+                return [null, null];
+            } else {
+                const coords = ethers.utils.defaultAbiCoder.decode(["int256", "int256"], binCoords);
+                return [coords[0].toNumber(), coords[1].toNumber()];
+            }
         } catch(err) {
             console.log(err);
         }
     }
 
-    async getAddress() {
-        const signer = this.provider.getSigner();
+    async setIdCoords(id: number, lat: number, lon: number, signer?: Signer): Promise<TransactionResponse> {
+        if (lat < -900000000 || lat > 900000000) {
+            throw `latitude of ${lat} is outside allowed range [-900000000, 900000000]`;
+        }
+        if (lon < -1800000000 || lon > 1800000000) {
+            throw `longitude of ${lon} is outside allowed range [-1800000000, 1800000000]`;
+        }
         try {
-            const address = await signer.getAddress();
-            return address;
+            if (signer === undefined) {
+                signer = this.provider.getSigner();
+            }
+            const signedContract = this.contract.connect(signer);
+            const binCoords = ethers.utils.defaultAbiCoder.encode(["int256", "int256"], [lat, lon]);
+            return await signedContract.updateCoordinate(id, binCoords);
         } catch(err) {
             console.log(err);
         }
-    }
-
-    async timer() {
-        await new Promise(r => setTimeout(r, 500));
     }
 }
-
-const p = new ContractIFace();
-//console.dir(p);
-//p.getAddress().then(res => {console.log(res)});
-p.getIdOwner(1234).then(res => {console.log(res)});
-p.getIdCoords(1234).then(res => {console.log(res)});
-
